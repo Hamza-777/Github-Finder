5f1f998a0cb9a4742723164f43c6edc0c220799f
Hamza-777 <hamzararrani77@gmail.com>
2020-09-22 17:17:49 +0530

    made some updates

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init GitHub
 const github = new GitHub;
 
+// Init UI
+const ui = new UI;
+
 // Search input
 const searchUser = document.getElementById('searchUser');
 
@@ -17,6 +20,7 @@ searchUser.addEventListener('keyup', (e) => {
                     // Show alert
                 } else {
                     // Show profile
+                    ui.showProfile(data.profile);
                 }
             })
     } else {
