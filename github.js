341a44b8e58d67546f41382ffc9686f0f2796219
Hamza-777 <hamzararrani77@gmail.com>
2020-09-22 19:44:45 +0530

    final commit/update/changes

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = '03f4188ca87e5533441f';
         this.client_secret = '1cc948d0fc0a086b5bda5f529a379bbe1377c976';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
