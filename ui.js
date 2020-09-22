3034addca507f1309f164767494f39aa62bbd9a5
Hamza-777 <hamzararrani77@gmail.com>
2020-09-22 17:18:24 +0530

    added ui.js for userinterface

---
+++ ui.js
@@ -0,0 +1,26 @@
+class UI {
+    constructor() {
+        this.profile = document.getElementById('profile');
+    }
+
+    showProfile(user) {
+        this.profile.innerHTML = `
+        <div class="card card-body mb-3">
+          <div class="row">
+            <div class="col-md-3">
+              <img class="img-fluid mb-2" src="${user.avatar_url}">
+              <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile</a>
+            </div>
+            <div class="col-md-9">
+              <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+              <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+              <span class="badge badge-success">Followers: ${user.followers}</span>
+              <span class="badge badge-info">Following: ${user.following}</span>
+              <br><br>
+              
+            </div>
+          </div>
+        </div>
+      `;
+    }
+}
